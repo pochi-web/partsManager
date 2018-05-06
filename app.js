b769c3345892ac09560080965e07d3924f41dd24
Kazunori Oyama <kazunori0326.t@gmail.com>
2018-05-06 23:46:08 +0000

    1:add modules for postgres. 2:made view and routes for account creation

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var createUser = require('./routes/createUser');
+
+var passport = require('passport');
 
 var app = express();
 
@@ -21,9 +24,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/createUser',createUser);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
