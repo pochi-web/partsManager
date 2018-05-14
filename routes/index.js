169f26577f5e73359a4d21abb556d6e2f7eace61
Kazunori Oyama <kazunori0326.t@gmail.com>
2018-05-14 22:50:32 +0000

    write to DB testing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var connect = require('./connect');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req,res,next){
+  var itemName = req.body.itemName;
+  var quantity = req.body.quantity;
+  var query = 'INSERT INTO itemList(itemName,quantity) VALUES("'+ itemName +'",'+' "' + quantity + '")';
+
+  connect.query(query,function(err,rows){
+    res.redirect('/');
+    console.log(itemName);
+    console.log(quantity);
+  });
+  
 });
 
 module.exports = router;
