8c22d4327d35c7b267393b82975dcca9be69482c
Kazunori Oyama <kazunori0326.t@gmail.com>
2018-05-16 23:31:15 +0000

    add moment package and edit queries

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var connect = require('./connect');
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,7 +12,8 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req,res,next){
   var itemName = req.body.itemName;
   var quantity = req.body.quantity;
-  var query = 'INSERT INTO itemList(itemName,quantity) VALUES("'+ itemName +'",'+' "' + quantity + '")';
+  var last_modified = moment().format('YYYY-MM-DD HH:mm:ss');
+  var query = 'INSERT INTO itemList(itemName,quantity,last_modified) VALUES("'+ itemName +'",'+' "' + quantity + '",'+'"'+last_modified+'")';
 
   connect.query(query,function(err,rows){
     res.redirect('/');
