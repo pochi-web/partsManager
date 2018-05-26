4bb09cd59c51ef29c0af3da8c5bb91ed6d0d5073
Kazunori Oyama <kazunori0326.t@gmail.com>
2018-05-26 06:21:18 +0000

    matched edit model and add model

--- routes/edit.js
+++ routes/edit.js
@@ -19,10 +19,18 @@ router.get('/:id',function(req,res,next){
 router.post('/:id',function(req,res,next){
     var id = req.params.id;
     var newId = req.body.id;
+    var category = req.body.category;
+    var producer = req.body.producer; 
     var itemName = req.body.itemName;
+    var product_number = req.body.product_number;
+    var rack = req.body.rack;
+    var palette = req.body.palette;
+    var vender = req.body.vender;
+    var used = req.body.used;
+    var brand_new = req.body.new;
     var quantity = req.body.quantity;
     var last_modified = moment().format('YYYY-MM-DD HH:mm:ss');
-    var query = 'UPDATE itemList SET id="'+newId+'", itemName="'+itemName+'", quantity="'+quantity+'",last_modified="'+last_modified+'" WHERE id="'+id+'"';    
+    var query = 'UPDATE itemList SET id="'+newId+'", category="'+category+'", producer="'+producer+'",itemName="'+itemName+'", product_number="'+product_number+'",rack="'+rack+'",palette="'+palette+'",vender="'+vender+'",used="'+used+'",brand_new="'+brand_new+'",quantity="'+quantity+'",last_modified="'+last_modified+'" WHERE id="'+id+'"';    
     connect.query(query,function(err,rows){
         console.log(query);
         res.redirect('/');
